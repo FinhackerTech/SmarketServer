05b03bc4bfd128ec02d101bc5eb696724396af54
lyk <1037400635@qq.com>
2022-03-13 20:46:06 +0800

    lyk test

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -50,69 +50,69 @@ node("yxw") {
 
 
 
-    stage('clone from gitlab into slave\'s workspace') {
-        echo "workspace: ${workspace}"
-        git branch: "${git_branch}", url: "${git_repository}"
-    }
-
-
-    stage('cd to build context') {
-        echo "the context now is:"
-        sh "ls -al"
-        sh "cd ${workspace}"
-        echo "cd to build context, now the context is:"
-        sh "ls -al"
-
-    }
-    stage('build jar on slave machine, jacoco file generated') {
-        echo 'jdk should be 11'
-        sh 'java --version'
-
-        sh 'mvn --version'
-        sh "mvn  clean package jacoco:report -Dmaven.test.failure.ignore=true"
-        echo "build finish on ${vm_ip}"
-
-    }
-
-    stage( 'testing, using jacoco' ) {
-        jacoco (
-                execPattern: '**/target/jacoco.exec',
-                classPattern: '**/classes',
-                sourcePattern: '**/src/main/java',
-                exclusionPattern: '**/src/test*',
-//                inclusionPattern: '**/com/hel/auto/service/*.class,**/com/hel/auto/controller/*.class',
-        )
-    }
-
-
-    stage("build docker image"){
-        sh "docker build -t ${IMAGE_NAME} ."
-    }
-
-    stage("login to dockerhub"){
-        withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_KEY', passwordVariable: 'password', usernameVariable: 'username')]) {
-            sh 'docker login -u $username -p $password'
-        }
-    }
-
-    stage("push to dockerhub"){
-        echo "begin push to dockerhub"
-        sh "docker image tag ${IMAGE_NAME_WITH_TAG} lyklove/${IMAGE_NAME_WITH_TAG}"
-        sh "docker image push lyklove/${IMAGE_NAME_WITH_TAG}"
-    }
-    stage("clean previous image and container"){
-        sh "docker container rm -f ${CONTAINER_NAME}"
-        sh "docker image rm ${IMAGE_NAME_WITH_TAG}"
-        sh "docker image rm ${IMAGE_TO_RUN}"
-    }
-    stage( "pull image" ){
-        sh "docker pull  lyklove/${IMAGE_NAME_WITH_TAG}"
-    }
-    stage("run container") {
-        sh "docker image ls"
-        sh "docker container run --name ${CONTAINER_NAME} --net=host  -d ${IMAGE_TO_RUN}"
-    }
-    stage("signal gitlab: deployed"){
+//    stage('clone from gitlab into slave\'s workspace') {
+//        echo "workspace: ${workspace}"
+//        git branch: "${git_branch}", url: "${git_repository}"
+//    }
+//
+//
+//    stage('cd to build context') {
+//        echo "the context now is:"
+//        sh "ls -al"
+//        sh "cd ${workspace}"
+//        echo "cd to build context, now the context is:"
+//        sh "ls -al"
+//
+//    }
+//    stage('build jar on slave machine, jacoco file generated') {
+//        echo 'jdk should be 11'
+//        sh 'java --version'
+//
+//        sh 'mvn --version'
+//        sh "mvn  clean package jacoco:report -Dmaven.test.failure.ignore=true"
+//        echo "build finish on ${vm_ip}"
+//
+//    }
+//
+//    stage( 'testing, using jacoco' ) {
+//        jacoco (
+//                execPattern: '**/target/jacoco.exec',
+//                classPattern: '**/classes',
+//                sourcePattern: '**/src/main/java',
+//                exclusionPattern: '**/src/test*',
+////                inclusionPattern: '**/com/hel/auto/service/*.class,**/com/hel/auto/controller/*.class',
+//        )
+//    }
+//
+//
+//    stage("build docker image"){
+//        sh "docker build -t ${IMAGE_NAME} ."
+//    }
+//
+//    stage("login to dockerhub"){
+//        withCredentials([usernamePassword(credentialsId: 'DOCKERHUB_KEY', passwordVariable: 'password', usernameVariable: 'username')]) {
+//            sh 'docker login -u $username -p $password'
+//        }
+//    }
+//
+//    stage("push to dockerhub"){
+//        echo "begin push to dockerhub"
+//        sh "docker image tag ${IMAGE_NAME_WITH_TAG} lyklove/${IMAGE_NAME_WITH_TAG}"
+//        sh "docker image push lyklove/${IMAGE_NAME_WITH_TAG}"
+//    }
+//    stage("clean previous image and container"){
+//        sh "docker container rm -f ${CONTAINER_NAME}"
+//        sh "docker image rm ${IMAGE_NAME_WITH_TAG}"
+//        sh "docker image rm ${IMAGE_TO_RUN}"
+//    }
+//    stage( "pull image" ){
+//        sh "docker pull  lyklove/${IMAGE_NAME_WITH_TAG}"
+//    }
+//    stage("run container") {
+//        sh "docker image ls"
+//        sh "docker container run --name ${CONTAINER_NAME} --net=host  -d ${IMAGE_TO_RUN}"
+//    }
+    stage("signal github: deployed"){
         def build = new Build(description: "build success!!!")
 
         updateGithubCommitStatus(build)
