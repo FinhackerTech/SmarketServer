94fd611c9cf2ae0f6bec10be56e94d56490e1b05
lyk <1037400635@qq.com>
2022-03-17 23:03:47 +0800

    lyk jenkins may fail

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,49 +1,49 @@
-class Build{
-    String description
-}
-
-def getRepoURL() {
-    sh "git config --get remote.origin.url > .git/remote-url"
-    return readFile(".git/remote-url").trim()
-}
-
-def getCommitSha() {
-    sh "git rev-parse HEAD > .git/current-commit"
-    return readFile(".git/current-commit").trim()
-}
-
-def updateGithubCommitStatus(build) {
-    // workaround https://issues.jenkins-ci.org/browse/JENKINS-38674
-    repoURL = getRepoURL()
-    commitSha = getCommitSha()
-
-    step([
-            $class: 'GitHubCommitStatusSetter',
-            reposSource: [$class: "ManuallyEnteredRepositorySource", url: repoURL],
-
-            commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitSha],
-            errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
-            statusResultSource: [
-                    $class: 'ConditionalStatusResultSource',
-                    results: [
-//                            [$class: 'AnyBuildResult', result: 'SUCCESS', state: 'SUCCESS', message: build.description]
-                            [$class: 'BetterThanOrEqualBuildResult', result: 'SUCCESS', state: 'SUCCESS', message: build.description],
-                            [$class: 'BetterThanOrEqualBuildResult', result: 'FAILURE', state: 'FAILURE', message: build.description],
-                            [$class: 'AnyBuildResult', state: 'FAILURE', message: 'Loophole']
-                    ]
-            ]
-    ])
-}
-
-void setBuildStatus(String message, String state) {
-    step([
-            $class: "GitHubCommitStatusSetter",
-            reposSource: [$class: "ManuallyEnteredRepositorySource", url: "git@github.com:FinhackerTech/SmarketServer.git"],
-            contextSource: [$class: "ManuallyEnteredCommitContextSource", context: "ci/jenkins/build-status"],
-            errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
-            statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
-    ]);
-}
+//class Build{
+//    String description
+//}
+//
+//def getRepoURL() {
+//    sh "git config --get remote.origin.url > .git/remote-url"
+//    return readFile(".git/remote-url").trim()
+//}
+//
+//def getCommitSha() {
+//    sh "git rev-parse HEAD > .git/current-commit"
+//    return readFile(".git/current-commit").trim()
+//}
+//
+//def updateGithubCommitStatus(build) {
+//    // workaround https://issues.jenkins-ci.org/browse/JENKINS-38674
+//    repoURL = getRepoURL()
+//    commitSha = getCommitSha()
+//
+//    step([
+//            $class: 'GitHubCommitStatusSetter',
+//            reposSource: [$class: "ManuallyEnteredRepositorySource", url: repoURL],
+//
+//            commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitSha],
+//            errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+//            statusResultSource: [
+//                    $class: 'ConditionalStatusResultSource',
+//                    results: [
+////                            [$class: 'AnyBuildResult', result: 'SUCCESS', state: 'SUCCESS', message: build.description]
+//                            [$class: 'BetterThanOrEqualBuildResult', result: 'SUCCESS', state: 'SUCCESS', message: build.description],
+//                            [$class: 'BetterThanOrEqualBuildResult', result: 'FAILURE', state: 'FAILURE', message: build.description],
+//                            [$class: 'AnyBuildResult', state: 'FAILURE', message: 'Loophole']
+//                    ]
+//            ]
+//    ])
+//}
+//
+//void setBuildStatus(String message, String state) {
+//    step([
+//            $class: "GitHubCommitStatusSetter",
+//            reposSource: [$class: "ManuallyEnteredRepositorySource", url: "git@github.com:FinhackerTech/SmarketServer.git"],
+//            contextSource: [$class: "ManuallyEnteredCommitContextSource", context: "ci/jenkins/build-status"],
+//            errorHandlers: [[$class: "ChangingBuildStatusErrorHandler", result: "UNSTABLE"]],
+//            statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
+//    ]);
+//}
 
 
 node("yxw") {
